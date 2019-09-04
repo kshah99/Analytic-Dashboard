5ff996124bb7a2b9e6cd9fd6af90034dfe959aad
Kerin <kshah99@hawk.iit.edu>
2019-09-04 13:10:38 -0500

    Analytic Files Added

---
+++ AnalyticDashboard/AnalyticDashboard/WebContent/js/windowSize.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+function getWindowDimension() {
+	    // Get the dimensions of the viewport
+	    var width = window.innerWidth ||
+	                document.documentElement.clientWidth ||
+	                document.body.clientWidth;
+	    var height = window.innerHeight ||
+	                 document.documentElement.clientHeight ||
+	                 document.body.clientHeight;
+	    
+	    return height + "||" + width;
+}
